d056c068fa9a2583ae755adaceaa3e1e0c8d1a03
Carlos Herrera <carlos.cervantes.cp@gmail.com>
2022-01-08 23:24:13 -0600

    EB-9 Added a successful verification template when a customer verifies their account

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema, User } from './schemas/user.schema';
+import { UserAuthenticationListener } from './listeners/user-authentication.listener';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
-  providers: [UserService],
+  providers: [UserService, UserAuthenticationListener],
   exports: [UserService],
 })
 export class UserModule {}
